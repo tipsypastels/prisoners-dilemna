8bc2300af612618bcaed6c1c4f981548b98e5a77
tipsypastels <dakotasankey@gmail.com>
2025-01-15 19:45:34 -0800

    minify core

--- js/core/rolldown.config.js
+++ js/core/rolldown.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "rolldown";
+import { minify } from "rollup-plugin-esbuild";
 
 export default defineConfig({
   input: "src/index.ts",
   output: {
     file: "dist/index.js",
   },
+  plugins: [/** @type {any} */ (minify())],
 });
