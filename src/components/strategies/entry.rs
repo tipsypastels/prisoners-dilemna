f838b3704b897877a2532738978f277792317275
tipsypastels <dakotasankey@gmail.com>
2025-01-15 20:13:37 -0800

    selection handles

--- src/components/strategies/entry.rs
+++ src/components/strategies/entry.rs
@@ -1,22 +1,22 @@
-use crate::models::Strategy;
+use super::StrategiesListSelectionHandle;
+use crate::{models::Strategy, tw};
 use yew::prelude::*;
 
 #[derive(Debug, Properties, PartialEq)]
 pub struct StrategiesEntryProps {
+    pub index: usize,
     pub strategy: Strategy,
     #[prop_or_default]
-    pub is_selected1: bool,
-    #[prop_or_default]
-    pub is_selected2: bool,
-    #[prop_or_default]
-    pub onselect: Option<Callback<()>>,
+    pub selections: Option<(StrategiesListSelectionHandle, StrategiesListSelectionHandle)>,
 }
 
 #[function_component]
 pub fn StrategiesEntry(props: &StrategiesEntryProps) -> Html {
+    let index = props.index;
     let strategy = &props.strategy;
+
     html! {
-        <li class="mb-4 p-4 shadow-lg transition hover:-translate-y-1">
+        <li class="mb-4 p-4 shadow-lg">
             <div class="flex rounded-lg border-2 border-dashed border-lime-500 p-4">
                 <div class="grow">
                     <h2 class="mb-2 text-xl font-bold">
@@ -27,7 +27,49 @@ pub fn StrategiesEntry(props: &StrategiesEntryProps) -> Html {
                         {strategy.desc()}
                     </div>
                 </div>
+
+                if let Some((s1, s2)) = &props.selections {
+                    <div class="flex gap-2">
+                        <SelectButton
+                            label={"Player 1"}
+                            class="border-teal-700 text-teal-700 disabled:bg-teal-700"
+                            index={index}
+                            selection={s1.clone()}
+                        />
+
+                        <SelectButton
+                            label={"Player 2"}
+                            class="border-indigo-700 text-indigo-700 disabled:bg-indigo-700"
+                            index={index}
+                            selection={s2.clone()}
+                        />
+                    </div>
+                }
             </div>
         </li>
+    }
+}
+
+#[derive(Debug, Properties, PartialEq)]
+struct SelectButtonProps {
+    label: &'static str,
+    class: &'static str,
+    index: usize,
+    selection: StrategiesListSelectionHandle,
+}
+
+#[function_component]
+fn SelectButton(props: &SelectButtonProps) -> Html {
+    let base_class = tw!("border-2 border-dashed font-bold px-6 rounded-lg disabled:border-solid disabled:text-white");
+    let index = props.index;
+    let selection = props.selection.clone();
+    let selected = selection.is_some_and(|i| i == index);
+    let onclick = move |_| selection.set(Some(index));
+
+    html! {
+        // TODO: Use `tailwind-merge`.
+        <button class={classes!(props.class, base_class)} onclick={onclick} disabled={selected}>
+            {props.label}
+        </button>
     }
 }
--- src/components/strategies/list.rs
+++ src/components/strategies/list.rs
@@ -5,26 +5,37 @@ use crate::{
 };
 use yew::prelude::*;
 
+pub type StrategiesListSelectionHandle = UseStateHandle<Option<usize>>;
+
 #[derive(Debug, Properties, PartialEq)]
-pub struct StrategiesListProps {}
+pub struct StrategiesListProps {
+    pub selection1: StrategiesListSelectionHandle,
+    pub selection2: StrategiesListSelectionHandle,
+}
 
 #[function_component]
 pub fn StrategiesList(props: &StrategiesListProps) -> Html {
+    let selection1 = props.selection1.clone();
+    let selection2 = props.selection2.clone();
+
     let state = use_state_context();
     let strategies = use_memo(state.custom_strategies.clone(), |c| {
         NativeStrategy::ALL
             .into_iter()
             .map(Strategy::from)
             .chain(c.iter().map(Strategy::from))
+            .enumerate()
             .collect::<Vec<_>>()
     });
 
     let entries = strategies
         .iter()
-        .map(|s| {
+        .map(|(i, s)| {
             html! {
                 <StrategiesEntry
+                    index={i}
                     strategy={s.clone()}
+                    selections={(selection1.clone(), selection2.clone())}
                 />
             }
         })
